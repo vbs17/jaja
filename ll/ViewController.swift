dbf1f5fa2d8470d7c8f6d76bb5bf06af0b32e490
kei ikeuchi <sister@sister.local>
2016-10-28 21:05:07 +0900

    ,

--- ll/ViewController.swift
+++ ll/ViewController.swift
@@ -1,25 +1,80 @@
-//
-//  ViewController.swift
-//  ll
-//
-//  Created by kei ikeuchi on 2016/10/28.
-//  Copyright © 2016年 mycompany. All rights reserved.
-//
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    let engine = AVAudioEngine()
+    let player = AVAudioPlayerNode()
+    let audioEngine = AVAudioEngine()
+
+    
+    func play() {
+        if let url = NSBundle.mainBundle().URLForResource("sound-file", withExtension: "wav") {
+            do {
+                let audioFile = try AVAudioFile(forReading: url)
+                audioEngine.attachNode(player)
+                let mixer = audioEngine.mainMixerNode
+
+                audioEngine.connect(player,
+                                    to: mixer,
+                                    format: audioFile.processingFormat)
+                player.scheduleFile(audioFile, atTime: nil) {
+                    print("complete")
+                }
+                try audioEngine.start()
+                player.play()
+            } catch let error {
+                print(error)
+            }
+        } else {
+            print("File not found")
+        }
+    }
+    
+    func record() {
+        do {
+            let documentDir = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).first!
+            let filePath = NSURL(fileURLWithPath: documentDir + "/sample.caf")
+            let format = AVAudioFormat(commonFormat: .PCMFormatFloat32  , sampleRate: 44100, channels: 1 , interleaved: true)
+            let audioFile = try AVAudioFile(forWriting: filePath, settings: format.settings)
+       
+            let inputNode = engine.inputNode!
+            inputNode.installTapOnBus(0, bufferSize: 4096, format: nil) { (buffer, when) in
+                do {
+                    try audioFile.writeFromBuffer(buffer)
+                } catch let error {
+                    print("audioFile.writeFromBuffer error:", error)
+                }
+            }
+            
+            do {
+                try engine.start()
+            } catch let error {
+                print("engine.start() error:", error)
+            }
+        } catch let error {
+            print("AVAudioFile error:", error)
+        }
     }
 
+    @IBOutlet weak var rec: UIButton!
+    @IBAction func recGo(sender: AnyObject) {
+        record()
+    }
+    @IBOutlet weak var all: UIButton!
+    @IBAction func allGo(sender: AnyObject) {
+        play()
+    }
 
 }
 
